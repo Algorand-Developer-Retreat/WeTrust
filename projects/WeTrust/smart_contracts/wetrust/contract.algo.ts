d802f41d2554f06d9cd8e9fd64ad04b78ad27f52
VKappaKV <jacopo851@gmail.com>
2025-04-02 13:05:36 +0200

    finished contract

--- projects/WeTrust/smart_contracts/wetrust/contract.algo.ts
+++ projects/WeTrust/smart_contracts/wetrust/contract.algo.ts
@@ -1,15 +1,4 @@
-import {
-  Contract,
-  BoxMap,
-  Account,
-  uint64,
-  bytes,
-  Txn,
-  MutableArray,
-  Application,
-  Asset,
-  abimethod,
-} from "@algorandfoundation/algorand-typescript";
+import { Contract, BoxMap, Account, Txn, Application, Asset, abimethod, Global } from "@algorandfoundation/algorand-typescript";
 
 export class Wetrust extends Contract {
   /**
@@ -25,43 +14,78 @@ export class Wetrust extends Contract {
 
   adjacencyList = BoxMap<Account, Account[]>({ keyPrefix: "adjacency_list" }); // this or use a dynamic array as adjacency list.
 
-  addTrustedApp(appToAdd: Application): void {
+  /**
+   * method to add trusted applications, trusted ASAs and trusted peers to your list
+   * @param {Application[]} apps - list of trusted applications to add; leave empty if not adding any
+   * @param {Asset[]} assets - list of trusted ASAs to add; leave empty if not adding any
+   * @param {Account[]} peers - list of trusted peers to add; leave empty if not adding any
+   */
+
+  add(apps: Application[], assets: Asset[], peers: Account[]): void {
     if (!this.trustedApp(Txn.sender).exists) {
       this.trustedApp(Txn.sender).value = [];
     }
-    let trustedAppList = this.trustedApp(Txn.sender).value;
-    trustedAppList = [...trustedAppList, appToAdd];
-  }
-
-  addTrustedASA(asaToAdd: Asset): void {
     if (!this.trustedASA(Txn.sender).exists) {
       this.trustedASA(Txn.sender).value = [];
     }
-    let trustedASAlist = this.trustedASA(Txn.sender).value;
-    trustedASAlist = [...trustedASAlist, asaToAdd];
+    if (!this.adjacencyList(Txn.sender).exists) {
+      this.adjacencyList(Txn.sender).value = [];
+    }
+
+    // can you pass an empty array to the function?
+    if (apps.length !== 0) {
+      let trustedAppList = this.trustedApp(Txn.sender).value;
+      this.trustedApp(Txn.sender).value = trustedAppList;
+      trustedAppList = [...trustedAppList, ...apps];
+    }
+
+    if (assets.length !== 0) {
+      let trustedASAlist = this.trustedASA(Txn.sender).value;
+      trustedASAlist = [...trustedASAlist, ...assets];
+      this.trustedASA(Txn.sender).value = trustedASAlist;
+    }
+
+    if (peers.length !== 0) {
+      let adjacencyList = this.adjacencyList(Txn.sender).value;
+      adjacencyList = [...adjacencyList, ...peers];
+      this.adjacencyList(Txn.sender).value = adjacencyList;
+    }
   }
 
-  removeApp(appToRemove: Application): void {
-    const trustedAppList = this.trustedApp(Txn.sender).value;
-    let newTrustedAppList: Application[] = [];
-    for (let i = 0; i < trustedAppList.length; i++) {
-      if (trustedAppList[i] !== appToRemove) {
-        newTrustedAppList = [...newTrustedAppList, trustedAppList[i]];
+  remove(app: Application, asset: Asset, peer: Account): void {
+    if (app !== Application(0)) {
+      const appList = this.trustedApp(Txn.sender).value;
+      let newAppList: Application[] = [];
+      for (let i = 0; i < appList.length; i += 1) {
+        if (appList[i] !== app) {
+          newAppList.push(appList[i]);
+        }
       }
+      this.trustedApp(Txn.sender).value = newAppList;
     }
-    this.trustedApp(Txn.sender).value = newTrustedAppList;
+    if (asset !== Asset(0)) {
+      const assetList = this.trustedASA(Txn.sender).value;
+      let newAssetList: Asset[] = [];
+      for (let i = 0; i < assetList.length; i += 1) {
+        if (assetList[i] !== asset) {
+          newAssetList.push(assetList[i]);
+        }
+      }
+      this.trustedASA(Txn.sender).value = newAssetList;
+    }
+    if (peer !== Global.zeroAddress) {
+      const peerList = this.adjacencyList(Txn.sender).value;
+      let newPeerList: Account[] = [];
+      for (let i = 0; i < peerList.length; i += 1) {
+        if (peerList[i] !== peer) {
+          newPeerList.push(peerList[i]);
+        }
+      }
+      this.adjacencyList(Txn.sender).value = newPeerList;
+    }
   }
 
-  removeASA(asaToRemove: Asset): void {
-    const trustedASAlist = this.trustedASA(Txn.sender).value;
-    let newTrustedASAlist: Asset[] = [];
-    for (let i = 0; i < trustedASAlist.length; i++) {
-      if (trustedASAlist[i] !== asaToRemove) {
-        newTrustedASAlist = [...newTrustedASAlist, trustedASAlist[i]];
-      }
-    }
-    this.trustedASA(Txn.sender).value = newTrustedASAlist;
-  }
+  //////////////
 
   @abimethod({ readonly: true })
   getTrustedApp(account: Account): Application[] {
@@ -71,5 +95,10 @@ export class Wetrust extends Contract {
   @abimethod({ readonly: true })
   getTrustedASA(account: Account): Asset[] {
     return this.trustedASA(account).value;
+  }
+
+  @abimethod({ readonly: true })
+  getAdjacencyList(account: Account): Account[] {
+    return this.adjacencyList(account).value;
   }
 }
